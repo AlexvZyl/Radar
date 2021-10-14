43f89775f59e7a764b43968ca4c694fb43e31898
AlexEnerdyne <81622310+AlexEnerdyne@users.noreply.github.com>
2021-10-14 21:34:16 +0200

    Working on binary file processing.

--- Environments/RadarProcessing/binaryProcessor.jl
+++ Environments/RadarProcessing/binaryProcessor.jl
@@ -19,8 +19,8 @@ using FloatingTableView
 
 # Data file consists of rpds, I, Q, rpds, I, Q ...
 #   193 pulse header (rpds) bytes
-#   8192 I-data bytes
-#   8192 Q-data bytes
+#   8192 I-data units, 16bit each.
+#   8192 Q-data units, 16bit each.
 #   ...
 #   193 pulse header (rpds) bytes
 #   8192 I-data bytes
@@ -29,10 +29,12 @@ using FloatingTableView
 
 binaryFormat = DataFrame(
     # Component     Size.
-    RPDS            = 193,  # Bytes.
-    I               = 8192, # Bytes.
-    Q               = 8192, # Bytes.
-    blocks          = 512   # Total blocks.
+    RPDS            = 193,      # 8 Bit.
+    I               = 8192,     # 16 Bit.
+    Q               = 8192,     # 16 Bit.
+    blocks          = 512,      # Total blocks.
+    fileSize        = (193+8192*2*2) *
+                       512      # 8 Bit.
 )
 
 # ============================================================ #
@@ -81,7 +83,43 @@ RPDS = DataFrame(
 # Binary processing script.
 # ============================================================ #
 
-fileSizeBytes = (binaryFormat.I+binaryFormat.Q+binaryFormat.RPDS)[1] * binaryFormat.blocks[1];
+# Create a dataframe to contain the data.
+v_RPDS = [Vector{UInt8}(undef, binaryFormat.RPDS[1]) for _ in 1:binaryFormat.blocks[1]]
+v_I = [Vector{UInt16}(undef, (binaryFormat.I[1])) for _ in 1:binaryFormat.blocks[1]]
+v_Q = [Vector{UInt16}(undef, (binaryFormat.Q[1])) for _ in 1:binaryFormat.blocks[1]]
+rawDataDF = DataFrame(
+    RPDS    = v_RPDS,
+    I       = v_I,
+    Q       = v_Q
+)
+
+# Read the byte data into a tepmp variable.
+file = "Environments\\RadarProcessing\\RadarData-Binary\\Rhino\\Capture_1000.bin";
+readVariable = Vector{UInt8}(
+    undef,
+    binaryFormat.I[1]*2 + binaryFormat.Q[1]*2 + binaryFormat.RPDS[1]
+)
+read!(file, readVariable)
+
+# Store RPDS.
+rawDataDF.RPDS[1] = readVariable[1:binaryFormat.RPDS[1]]
+# Store I samples.
+offset = binaryFormat.RPDS[1]
+rawDataDF.I[1] = reinterpret(UInt16, readVariable[offset+1:offset+binaryFormat.I[1]*2])
+offset += binaryFormat.I[1]*2;
+rawDataDF.Q[1] = reinterpret(UInt16, readVariable[offset+1:offset+binaryFormat.Q[1]*2])
+
+# ============================================================ #
+# Plotting.
+# ============================================================ #
+
+f = Figure()
+
+ax = Axis(f[1, 1], xlabel = "I Channel", ylabel = "Value",
+    title = "Pulse 0 For Rhino Bin")
+scatter!(rawDataDF.I[1])
+
+display(f)
 
 # ============================================================ #
 println("EOS.")
