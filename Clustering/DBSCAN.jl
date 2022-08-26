d041857187acddacd013d2948df2ce166ecad98b
Alex-vZyl <alexandervanzyl@protonmail.com>
2022-08-26 15:59:07 +0200

    Using JDL to save data

--- Clustering/DBSCAN.jl
+++ Clustering/DBSCAN.jl
@@ -1,4 +1,5 @@
 using Clustering
+using JLD 
 
-
-
+doppler_fft_matrix = load("Data/Test/B210_SAMPLES_Test_012.jld")["Doppler FFT Matrix"]
+display(doppler_fft_matrix)
--- Clustering/RawToDopplerMap.jl
+++ Clustering/RawToDopplerMap.jl
@@ -1,6 +1,8 @@
 # Processes the raw IQ samples from the SDR and stores the comples FFT results
 # in a file so that the clustering can take place.
 
+using JLD
+
 # Includes.
 include("../Utilities/MakieGL/PlotUtilities.jl")
 include("../SDR-Processing/Source/Waveforms/LFM.jl")
@@ -17,9 +19,9 @@ file_number 	= "012"
 # Fixed meta data.
 path 			= "/home/alex/GitHub/SDR-Interface/build/Data/"
 file_prefix 	= "B210_SAMPLES_" * folder * "_"
-file 			= path * folder * "/" * file_prefix * file_number
-file_bin 		= file * ".bin"
-file_txt		= file * ".txt"	
+file 			= file_prefix * file_number
+file_bin 		= path * folder * "/" * file * ".bin"
+file_txt		= path * folder * "/" * file * ".txt"	
 
 # Load the metadata.
 meta_data = load_meta_data(file_txt)
@@ -46,15 +48,13 @@ doppler_fft_matrix, range_vector, velocity_vector = plotDopplerFFT(false, pc_sig
 			                                        xRange = meta_data.max_range, yRange = 5, nWaveSamples=meta_data.wave_sample_count, plotDCBin = false, plotFreqLines = false, freqVal = 100000,
                                                     removeClutter = true, rawImage = false, return_doppler_fft = true)
 
-# Convert to dB.
-doppler_fft_matrix = 20*log10.(abs.(doppler_fft_matrix))
-
 # Debug with plot.
-figure = Figure()
-axis = Axis(figure[1,1])
-hm = heatmap!(figure[1, 1], range_vector, velocity_vector, doppler_fft_matrix, colorrange = [10, 20])
-display(figure)
+# figure = Figure()
+# axis = Axis(figure[1,1])
+# hm = heatmap!(figure[1, 1], range_vector, velocity_vector, doppler_fft_matrix, colorrange = [10, 20])
+# display(figure)
 
 # Save the results to a file.
-destination_folder = "Data/"
-destination_file = file_bin  
+destination_folder = "Data/" * folder * "/"
+destination_file = destination_folder * file * ".jld"  
+save(destination_file, "Doppler FFT Matrix", doppler_fft_matrix)
