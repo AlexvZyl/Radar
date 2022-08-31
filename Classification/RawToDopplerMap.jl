f1201df5e1cc48b727fd5184c1d5a120fe7d11e5
Alex-vZyl <alexandervanzyl@protonmail.com>
2022-08-31 22:18:52 +0200

    Fix file saveing location

--- Classification/RawToDopplerMap.jl
+++ Classification/RawToDopplerMap.jl
@@ -8,20 +8,14 @@ file_prefix 	= "/B210_SAMPLES_" * folder * "_"
 file            = path * folder * file_prefix * file_number
 
 # Calculate doppler data.
-doppler_fft_matrix, distance_vector, velocity_vector = calculate_doppler_map(file)
+doppler_fft_matrix, distance_vector, velocity_vector = calculate_doppler_map(abspath(file))
 
 # Destination file.                                                    
-destination_folder = "Data/" * folder * "/"
-destination_file = destination_folder * file * ".jld"  
+destination_folder = "Data/EntireDopplerMap/" * folder * "/"
+destination_file = relpath(destination_folder * file_prefix * file_number * ".jld")
 
 # Save the data to file.
 save(destination_file, "Doppler FFT Matrix", doppler_fft_matrix, 
                        "Velocity", velocity_vector,
                        "Distance", distance_vector)
 
-# Debug with plot.
-figure = Figure()
-axis = Axis(figure[1,1])
-doppler_fft_matrix = 20*log10.(abs.(doppler_fft_matrix))
-hm = heatmap!(figure[1, 1], distance_vector, velocity_vector, doppler_fft_matrix, colorrange = [15, 20])
-display(figure)
