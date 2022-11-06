6855b25556b49c0400909fa71660a0d1621e322d
Alex-vZyl <alexandervanzyl@protonmail.com>
2022-11-06 12:11:33 +0200

    Minor

--- Classification/DopplerFramesViewer.jl
+++ Classification/DopplerFramesViewer.jl
@@ -4,11 +4,11 @@ using Clustering
 include("DopplerMap.jl")
 
 # Meta data.
-folder 			 = "WalkingTowards"
-file_number 	 = "000"
+folder 			 = "JoggingAway_Elevated_90deg_Stick"
+file_number 	 = "001"
 
 # Fixed metdata.
-path 			 = "/home/alex/GitHub/Masters-Julia/Classification/Data/"
+path 			 = "/home/alex/GitHub/Radar/Classification/Data/"
 file_prefix 	 = "/B210_SAMPLES_" * folder * "_"
 
 # Load doppler frames.
@@ -18,11 +18,5 @@ doppler_frames   = file_data["Doppler FFT Frames"]
 distance_vector  = file_data["Distance"]
 velocity_vector  = file_data["Velocity"]
 
-# Load doppler clustering.
-clusters_file    = path * "DopplerClustering/" * folder * file_prefix * file_number * ".jld"
-clusters_file_data = load(clusters_file) 
-doppler_clusters = clusters_file_data["Clustering Result"]
-adjacency_matrix = clusters_file_data["Adjacency Matrix"]
-
 # Render.
-animate(doppler_frames, distance_vector, velocity_vector, snr_threshold = 0, clusters = doppler_clusters, adjacency_matrix = adjacency_matrix)
+animate(doppler_frames, distance_vector, velocity_vector, snr_threshold = 0)
