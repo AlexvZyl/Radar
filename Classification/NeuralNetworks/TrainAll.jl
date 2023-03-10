d28bce27feee6e93fb61bb89cf38d92c629bc5dd
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-03-10 15:44:26 +0200

    Larger runs

---
+++ Classification/NeuralNetworks/TrainAll.jl
@@ -0,0 +1,18 @@
+include("Trainer.jl")
+
+persons = [ 1,2 ]
+temporal = [ false,true ]
+frames = [ 1,3,7,10,15,20 ]
+models = []
+model = LeNet5Adapted
+batchsize = 8
+
+# Run all itertions of the testing.
+for p in persons
+    for t in temporal
+        for f in frames
+            frames_folder = string(f, "-Frames")
+            train(model, split = 0.7, batchsize = batchsize, epochs = 1000, checktime = 100, infotime = 1, temporal=t, persons=p, frames_folder = frames_folder)
+        end
+    end
+end
