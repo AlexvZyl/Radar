4c09dde023deed2156d78cb048b5538cb0e41f10
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-04-10 15:30:58 +0200

    Add FOPEN models

--- FOPEN/Figures.jl
+++ FOPEN/Figures.jl
@@ -1,33 +1,71 @@
 include("../Utilities/MakieGL/MakieGL.jl")
 include("Models.jl")
 
-function get_model_string(model::Function)
-    if model == Weissberger
-        return "Weissberger"
-    elseif model == FITUR_in_leaf
-        return "FITURE (in-leaf)"
-    elseif model == FITUR_out_of_leaf
-        return "FITURE (out-of-leaf)"
-    end
+function get_colors()
+    return [ :blue, :red, :orange, :purple, :cyan, :teal, :yellow ]
 end
 
-function generate_fopen_figure(model::Function, range::Number, resolution::Number, freq)
+function generate_fopen_figures(range::Number, resolution::Number, freq)
+    # Get results.
+    models = get_models()
     d = resolution:resolution:range
-    attenuation = [ model.(f, d) for f in freq ]
-    fig = Figure(resolution = (1920,1080))
-    ax = Axis(fig[1,1], xlabel="Distance (m)", ylabel="Signal Power (dB)", title="$(get_model_string(model)) FOPEN Attenuation")
-    for (i,a) in enumerate(attenuation)
-        scatterlines!(d, a, linewidth=lineThickness, markersize=dotSize, label="$(freq[i]) Hz")
+    results = [ [ attenuation(m, d, f) for f in freq ] for m in models ]
+     
+    # Setup figure.
+    fig = Figure(resolution = (2560,1440))
+    ax = Axis(fig[1,1], xlabel="Distance (m)", ylabel="Signal Power (dB)", title="FOPEN Models")
+    vlines!([0], color=:black, linewidth=3)
+    hlines!([0], color=:black, linewidth=3)
+
+    # Setup legend grid.
+    legend_grid = GridLayout()
+    legend_grid[1:2,1] = [ Axis(fig; leftspinevisible=false, rightspinevisible=false, topspinevisible=false, bottomspinevisible=false, xgridvisible=false, ygridvisible=false, xticklabelsvisible=false, yticklabelsvisible=false, xticksvisible=false, yticksvisible=false) for _ in 1:2 ]
+    fig.layout[1,2] = legend_grid
+
+    # Plot results.
+    colors = get_colors()
+    for (m,model) in enumerate(models)
+        for (i,_) in enumerate(freq)
+            scatterlines!(ax, d, results[m][i], markersize=dotSize*3, linewidth=2, marker=model.marker, color=colors[i], linestyle=model.line_style)
+        end
     end
-    xlims!(-5, range+5)
-    axislegend(ax)
-    file = get_model_string(model) * "_Models.pdf"
-    save(file, fig)
+
+    # Manually create legend.
+    points = [ Point2f(-2, 0.5), Point2f(1, 0.5) ]
+    # Frequencies.
+    Legend(
+        legend_grid[1,1],
+        [ LineElement(color=colors[i], linewidth=8, linepoints=points) for (i,_) in enumerate(freq) ],
+        [ string("  ", f/1e9) for f in freq ],
+        "Frequencies (GHz)",
+        valign = :bottom,
+        labelfont = "Latin Modern Math",
+        titlefont = "Latin Modern Math",
+        padding = 30,
+        labelsize=45,
+        titlesize=50
+    )
+    # Models.
+    Legend(
+        legend_grid[2,1],
+        [ legend(m, points) for m in models ],
+        [ string(" ", model.string) for model in models ],
+        "Models",
+        valign = :top,
+        labelfont = "Latin Modern Math",
+        titlefont = "Latin Modern Math", 
+        padding = (50,30,30,30),
+        labelsize=45,
+        titlesize=50
+    )
+
+    # Format and save.
+    xlims!(d[1]-5, d[end]+5)
+    save("FOPEN_Models.pdf", fig)
 end
 
+# Run script.
 freq = [800e6, 1.2e9, 5e9, 10e9, 20e9, 50e9]
 distance = 100
-resoltion = 0.5
-generate_fopen_figure(Weissberger, distance, resoltion, freq)
-generate_fopen_figure(FITUR_in_leaf, distance, resoltion, freq)
-generate_fopen_figure(FITUR_out_of_leaf, distance, resoltion, freq)
+resolution = 5
+generate_fopen_figures(distance, resolution, freq)
--- FOPEN/Models.jl
+++ FOPEN/Models.jl
@@ -15,3 +15,27 @@ end
 function FITUR_out_of_leaf(f::Number, d::Number)
     -dB(26.6 * f^(-0.2) * d^0.5)
 end
+
+mutable struct Model
+    func
+    line_style
+    marker
+    string
+end
+
+function get_models()
+    return [
+        Model(Weissberger, :solid, :circle, "Weissberger"),
+        Model(FITUR_out_of_leaf, :solid, :cross, "FITUR (out-of-leaf)"),
+        Model(FITUR_in_leaf, :solid, :diamond, "FITUR (in-leaf)")
+    ]
+end
+
+function attenuation(model::Model, distance, freq)
+    return model.func.(freq, distance) 
+end
+
+function legend(model::Model, points)
+    marker_point = Point2f((points[1][1] + points[2][1]) / 2, points[1][2])
+    return [ LineElement(color=:black, linewidth=5, linestyle=model.line_style, linepoints=points), MarkerElement(color=:black, marker=model.marker, markersize=dotSize*5, points = [ marker_point ]) ]
+end
