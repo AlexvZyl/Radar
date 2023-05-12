42921b8bfb241cb8ec76d9ee3b5123d11cd4ca83
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-05-12 17:22:30 +0200

    Optimising with Bayesian sampler

--- Classification/RandomForests.jl
+++ Classification/RandomForests.jl
@@ -1,20 +1,22 @@
+using Flux: state
 using Base: product
+using Hyperopt
 using ScikitLearn
 using DecisionTree
 using Base.Threads
 using CUDA
+using Optim
 include("NeuralNetworks/NetworkUtils.jl")
 include("NeuralNetworks/Trainer.jl")
+
+global training_state = TrainingState()
+global current_epoch = 0
 
 struct TreeParameters
     n_trees
     min_samples_leaf
     min_samples_split
     min_purity_increase
-end
-
-function default_params()
-    TreeParameters(10, 1, 2, 0.0)
 end
 
 function create_model(params::TreeParameters)
@@ -28,10 +30,10 @@ end
 
 function grid()
     return Dict(
-        "n_trees" => 1:20,
-        "min_samples_leaf" => 1:5,
-        "min_samples_split" => 2:5,
-        "min_purity_increase" => 0:0.5:5
+        "n_trees" => 1:1:100,
+        "min_samples_leaf" => 1:1:100,
+        "min_samples_split" => 2:1:100,
+        "min_purity_increase" => LinRange(0,0.1,10)
     )
 end
 
@@ -61,38 +63,36 @@ function prepare_data(args::Args)
     return train_x, train_y, test_x, test_y
 end
 
-function train(model::RandomForestClassifier, train_x, train_y, test_x, test_y, state, epoch)
+function train(model::RandomForestClassifier, train_x, train_y, test_x, test_y, args)
+    global current_epoch
     fit!(model, train_x, train_y)
     train_acc, train_loss = evaluate(model, train_x, train_y)
     test_acc, test_loss = evaluate(model, test_x, test_y)
-    current = TrainingResults(train_acc, train_loss, test_acc, test_loss, epoch)
-    update(current, state, model, epoch=epoch, args=args)
+    current = TrainingResults(train_acc, train_loss, test_acc, test_loss, current_epoch)
+    update(current, training_state, model, epoch=current_epoch, args=args)
+    current_epoch += 1
+    return -(test_acc+train_acc)
 end
 
 function train_random_forests(args::Args)
     # Setup.
     train_x, train_y, test_x, test_y = prepare_data(args)
-    state = TrainingState()
-    state.timeout = Inf
-    grid_seach = grid()
-    # search = ScikitLearn.GridSearch.RandomizedSearchCV()
-    total_epochs = length(grid_seach["n_trees"])*length(grid_seach["min_samples_leaf"])*length(grid_seach["min_samples_split"])*length(grid_seach["min_purity_increase"])
+    training_state.timeout = Inf
+    grid_search = grid()
+    sampler = BOHB()
 
-    # Grid search.
-    epoch = 0
-    for n_trees in grid_seach["n_trees"]
-        for min_samples_leaf in grid_seach["min_samples_leaf"]
-            for min_samples_split in grid_seach["min_samples_split"]
-                @threads for min_puroty_increase in grid_seach["min_purity_increase"]
-                    model = create_model(TreeParameters(n_trees, min_samples_leaf, min_samples_split, min_puroty_increase))
-                    train(model, train_x, train_y, test_x, test_y, state, epoch) 
-                    epoch+=1
-                end
-                @info "Progress: $((epoch/total_epochs)*100)%"
-            end
-        end
+    @thyperopt for i = args.epochs,
+                   sampler = sampler,
+                   n_trees = grid_search["n_trees"],
+                   min_samples_leaf = grid_search["min_samples_leaf"],
+                   min_samples_split = grid_search["min_samples_split"],
+                   min_purity_increase = grid_search["min_purity_increase"]
+        params = TreeParameters(n_trees, min_samples_leaf, min_samples_split, min_purity_increase)
+        model = create_model(params)
+        return train(model, train_x, train_y, test_x, test_y, args)
     end
+
 end
 
-args = Args(tree=true, persons=1, split=0.7, frames_folder="5-Frames")
+args = Args(tree=true, persons=1, split=0.7, frames_folder="1-Frames", epochs=10000)
 train_random_forests(args)
